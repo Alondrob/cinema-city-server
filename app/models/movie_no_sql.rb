ea6aa7486bea4cb398b1b2dff2ed58ac559052f6
Alondrob <alondrob@gmail.com>
2022-02-20 11:55:10 -0500

    updated

--- app/models/movie_no_sql.rb
+++
@@ -1,9 +0,0 @@
-class Movie < ApplicationRecord
-    include Mongoid
-    searchkick
-    has_many :movie_actors
-    has_many :actors, through: :movie_actors
-
-    field :name
-    field :year
-end
